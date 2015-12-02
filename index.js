9470363cb53a20c88a5d4d9c04825f639cfb7763
Justin Dragos <justin.dragos@gmail.com>
2015-12-02 12:35:06 -0700

    Updated checker to not rely directly on yargs.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 require('babel-core/register');
 const checker = require('./checker').default;
+const args = yargs.usage('Usage: $0 [options]')
+  .describe('loose', 'Check only that the current node version is greater the required engine version')
+  .describe('switch', 'If the node version doesn\'t satisfy the engine version attempt to use the n pacakage manager to switch the current node version to a compliant version.')
+  .help('h')
+  .argv;
 
+const options = {
+  switch: args.switch,
+  loose: args.loose
+}
 try {
-  checker();
+  checker(options);
+  console.log('info: Node version matches the engine version');
 } catch (err) {
   console.log(err.message);
   process.exit(1);
